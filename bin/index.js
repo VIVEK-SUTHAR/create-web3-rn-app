9e4dad425f211ecef3a2ae4fab1d71a672f413f6
Vivek Suthar <sutharvivek18@gmail.com>
2023-12-05 15:35:50 +0530

    Add support for new architecture

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,22 @@
 #! /usr/bin/env node
 
 import chalk from "chalk";
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 import * as fs from "fs";
 import inquirer from "inquirer";
 import ora from "ora";
 import path from "path";
 import { promisify } from "util";
+const OLD_ARCH_REPO_URL = "https://github.com/VIVEK-SUTHAR/cryptoreact.git";
+const NEW_ARCH_REPO_URL =
+  "https://github.com/VIVEK-SUTHAR/cryptoreact-new-architecture.git";
+
+const DEV_BUILDS_DOWNLOAD_URL =
+  "https://github.com/VIVEK-SUTHAR/cryptoreact-new-architecture/releases/tag/release";
 const execAsync = promisify(exec);
 
 const init = () => {
   console.log(chalk.green.bold("Create Web3 React Native + Expo App"));
-  console.log();
   console.log(
     chalk.blue("Bootstrap your your next Web3 Mobile App in seconds")
   );
@@ -33,16 +38,27 @@ async function createWeb3RnApp() {
         message: "What describes your app?",
         default: "my cool web3 mobile app",
       },
+      {
+        type: "confirm",
+        name: "isNewArch",
+        message:
+          "Do you want to use new architecture?,(If you are not sure, select No)",
+        default: false,
+      },
     ]);
-    const spinner = ora("Creating your Web3 RN + Expo App...").start();
+    const spinner = ora("Creating your Web3 RN + Expo App...\n").start();
 
     const appName = answers.appName;
     const appDesc = answers.appDesc;
+    const isNewArch = answers.isNewArch;
 
-    console.log(chalk.blueBright("Settinng up your project.."));
-    await execAsync(
-      `git clone https://github.com/VIVEK-SUTHAR/cryptoreact.git ${appName}`
-    );
+    console.log(chalk.blueBright("Settinng up your project..\n"));
+
+    if (isNewArch) {
+      await execAsync(`git clone ${NEW_ARCH_REPO_URL} ${appName}`);
+    } else {
+      await execAsync(`git clone ${OLD_ARCH_REPO_URL} ${appName}`);
+    }
     process.chdir(appName);
 
     const packageJsonPath = "./package.json";
@@ -56,22 +72,17 @@ async function createWeb3RnApp() {
 
     upadteDetailsInConstants(appName, appDesc);
     updateAppJson(appName);
+    console.log(chalk.white.bold("Installing dependencies...\n"));
+    await execAsync(isYarnInstalled() ? "yarn" : "npm install");
     await setUpGit(appName);
     console.log(
-      chalk.green.bold("Success! Your Web3 React Native app is ready.")
+      chalk.green.bold("Success! Your Web3 React Native app is ready.\n")
     );
-    console.log(
-      chalk.white.bold("You can run your app by following below commands ")
-    );
-    console.log(chalk.white.bold(`Change directory`));
-    console.log(chalk.white.bold(`cd ${appName}`));
-    console.log(chalk.white.bold(`Install dependencies using npm or yarn`));
-    console.log(chalk.white.bold(`yarn`));
-    console.log(chalk.white.bold(`and then run yarn start`));
-
+    printInstrctions(appName, isNewArch);
     spinner.stop();
   } catch (error) {
     console.error("Error setting up the project", error);
+    process.exit(1);
   }
 }
 
@@ -111,7 +122,6 @@ async function setUpGit(appName) {
     const gitDirectory = path.join(clonedDirectory, ".git");
 
     if (fs.existsSync(gitDirectory)) {
-      console.log("Removing existing Git history...");
       fs.rmdirSync(gitDirectory, { recursive: true });
       console.log("Existing Git history removed.");
     }
@@ -120,5 +130,36 @@ async function setUpGit(appName) {
     console.log("Initialized a new Git repository.");
   } catch (error) {
     console.log("Error while initailing new repo", error);
+  }
+}
+
+function isYarnInstalled() {
+  try {
+    execSync("yarn --version", { stdio: "ignore" });
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+function printInstrctions(appName, isNewArch) {
+  console.log(chalk.white("To get started, run the following commands:\n"));
+  if (isNewArch) {
+    console.log(
+      chalk.white(
+        "You have opted for New Architecture, So Please Download the Custom Expo Go from below Link.\n"
+      )
+    );
+    console.log(
+      chalk.blueBright(`Download the Build from ${DEV_BUILDS_DOWNLOAD_URL}\n`)
+    );
+    console.log(
+      chalk.white("1. Install the App in Simulator or Physical Device\n")
+    );
+    console.log(chalk.white(`2. Run cd ${appName}\n`));
+    console.log(
+      chalk.white(`3. ${isYarnInstalled() ? "yarn start" : "npm run start"} \n`)
+    );
+    console.log(chalk.white(`4. Scan The QR Code and Start Building... \n`));
   }
 }
